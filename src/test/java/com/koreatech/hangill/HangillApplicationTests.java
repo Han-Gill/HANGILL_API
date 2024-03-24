54305264a1ede758aee6a57437a0cae424df21bc
Jung, SungOh <tryjso@gmail.com>
2024-03-24 15:19:17 +0900

    feat: Project Setting
    Spring Boot 3.24

---
+++ src/test/java/com/koreatech/hangill/HangillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreatech.hangill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
